2633c03db1004f399ba68fd3f38643e64b68498a
Alfred Wong <alfred.cl.wong@gmail.com>
2019-09-18 23:12:17 +0100

    updated template

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include<algorithm>
+#include<iostream>
+#include<vector>
+using namespace std;
+#define ll long long
+#define uint unsigned
+#define ull unsigned ll
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define fi first
+#define se second
+#define For(i,j,k) for (int i=(int)(j);i<=(int)(k);i++)
+#define Rep(i,j,k) for (int i=(int)(j);i>=(int)(k);i--)
+#define nosync ios::sync_with_stdio(false)
+int main(){
+
+}
