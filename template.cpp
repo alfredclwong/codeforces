e592788e9a4f36ad75f099dc502a97fefaa530ad
Alfred Wong <alfred.cl.wong@gmail.com>
2019-09-25 12:57:50 +0100

    added make_pair to macro list in template.cpp

--- template.cpp
+++ template.cpp
@@ -1,13 +1,14 @@
 #include<algorithm>
 #include<iostream>
 #include<vector>
+#include<numeric>
+#include<math.h>
 using namespace std;
 #define ll long long
-#define uint unsigned
-#define ull unsigned ll
 #define pii pair<int,int>
 #define pll pair<ll,ll>
 #define pb push_back
+#define mp make_pair
 #define fi first
 #define se second
 #define For(i,j,k) for (int i=(int)(j);i<(int)(k);i++)
