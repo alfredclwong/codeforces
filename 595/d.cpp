58524b9e9c271000241234ad71416c394c129917
Alfred Wong <alfred.cl.wong@gmail.com>
2019-10-23 17:24:05 +0100

    codeforces #595

--- 595/d.cpp
+++ 595/d.cpp
@@ -1,14 +1,17 @@
 #include<algorithm>
 #include<iostream>
-#include<vector>
-#include<numeric>
 #include<math.h>
+#include<numeric>
 #include<set>
+#include<tuple>
+#include<vector>
 using namespace std;
 #define ll long long
 #define ull unsigned long long
 #define pii pair<int,int>
 #define pll pair<ll,ll>
+#define puu pair<ull,ull>
+#define eb emplace_back
 #define pb push_back
 #define mp make_pair
 #define fi first
@@ -21,22 +24,36 @@ int main() {
     int n, k;
     cin >> n >> k;
     int l[n], r[n];
-    int X = 200000;
-    vector<int> x[X];
+    vector<tuple<int,int,int>> lr;
     For (i, 0, n) {
         cin >> l[i] >> r[i];
-        For (j, l[i] - 1, r[i]) {
-            x[j].pb(i);
-        }
-        l[i]--;
-        r[i]--;
+        lr.eb(l[i], 0, i); // trick: make sure ls go before rs
+        lr.eb(r[i], 1, i);
     }
-    For (i, 0, X) {
-        if (!x[i].empty()) {
-            cout << i << ' ';
-            for (int p : x[i])
-                cout << p << ' ';
-            cout << endl;
+    sort(begin(lr), end(lr));
+    int h = 0;
+    vector<int> ans;
+    set<pii> rs;
+    for (auto [_, t, i] : lr) {
+        if (t == 0) {
+            h++;
+            rs.insert({r[i], i});
+            if (h > k) {
+                auto z = *rbegin(rs);
+                rs.erase(z);
+                ans.pb(z.se);
+                h--;
+            }
+        } else {
+            if (rs.count({r[i], i})) {
+                h--;
+                rs.erase({r[i], i});
+            }
         }
     }
+    cout << ans.size() << endl;
+    for (int i : ans)
+        cout << i+1 << ' ';
+    cout << endl;
 }
+// soln by ioyeoa
