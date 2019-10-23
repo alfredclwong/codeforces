872bc39f27b9984f685c666ea7f5ae9f94299f72
Alfred Wong <alfred.cl.wong@gmail.com>
2019-10-23 17:23:47 +0100

    include tuple, added puu and eb macros, added solve() function

--- template.cpp
+++ template.cpp
@@ -1,14 +1,17 @@
 #include<algorithm>
 #include<iostream>
-#include<vector>
-#include<numeric>
 #include<math.h>
+#include<numeric>
 #include<set>
+#include<tuple>
+#include<vector>
 using namespace std;
 #define ll long long
 #define ull unsigned long long
 #define pii pair<int,int>
 #define pll pair<ll,ll>
+#define puu pair<ull,ull>
+#define eb emplace_back
 #define pb push_back
 #define mp make_pair
 #define fi first
@@ -16,7 +19,13 @@ using namespace std;
 #define For(i,j,k) for (int i=(int)(j);i<(int)(k);i++)
 #define Rep(i,j,k) for (int i=(int)(j);i>=(int)(k);i--)
 #define nosync ios::sync_with_stdio(false)
+void solve() {
+    
+}
 int main() {
     nosync;
-
+    int q = 1;
+    //cin >> q;
+    For (i, 0, q)
+        solve();
 }
